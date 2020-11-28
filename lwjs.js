4644858dc691d432a440813d68733a726c2e087f
macleen <55474201+macleen@users.noreply.github.com>
2020-11-28 10:59:53 +0100

    Add files via upload

--- lwjs.js
+++ lwjs.js
@@ -174,13 +174,7 @@
         //*************************************************************
         
         clone: function ( o ) { 
-        
-            var primitives =['string', 'number', 'boolean', 'symbol', null, undefined, 'undefined'];
-            if ( !primitives.includes( this.type_of( o ))) 
-                    if ( typeof( o[Symbol.iterator] || o[Symbol.asyncIterator]) === 'function' )
-                        return [...o];
-            return o; 
-        
+                return typeof( o[Symbol.iterator] || o[Symbol.asyncIterator]) === 'function' ? [...o] : o;
         },
 
         /************************************************************* 
